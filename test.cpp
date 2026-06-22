ffb3f5eb5c15c586de3724afec74677c3f8bd233
Yash <y.yashsinghal207@gmail.com>
2026-06-22 16:25:09 +0530

    Updated .gitignore file

--- test.cpp
+++ test.cpp
@@ -1,5 +1,31 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World";
-}
+    int age;
+    cout<<age; //garbage value
+
+// Size of the DATA TYPES
+
+//int -> 4 Byte
+//long long -> 8 byte
+//float -> 4 byte
+//double -> 8 byte
+//char -> 1 byte
+//bool -> 1 byte
+
+//-----------------------------------//
+//Conditional Statement
+//if(cond...){
+    //This will be executed
+    //}
+
+    int marks=50,leaves = 9;
+    if(marks >=50 && leaves <=10){
+        cout<<"Passed";
+    }
+    else{
+        cout<<"Fail";
+    }
+    
+}    
+
